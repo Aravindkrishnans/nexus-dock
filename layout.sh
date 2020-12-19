fb013f4a8640666094d2384c2c4dfce9b474b779
Aravindkrishnans <47204553+Aravindkrishnans@users.noreply.github.com>
2020-12-19 19:14:55 +0530

    Update layout.sh

--- layout.sh
+++ layout.sh
@@ -2,3 +2,4 @@
 
 curl -O -u admin:admin123 http://13.232.84.146:8081/repository/maven-Test/com/web_test/web_test_nexus/1.1/web_test_nexus-1.1.war
 docker build -t aravind:v1 .
+docker run -itd -p 8080:8080 -name ak -t aravind:v1
